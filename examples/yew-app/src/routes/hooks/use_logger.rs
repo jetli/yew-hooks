6731a048e30622e270e267f146eae26289f5aca8
Jet Li <jing.i.qin@icloud.com>
2022-03-11 17:00:16 +0800

    Update demo comments

--- examples/yew-app/src/routes/hooks/use_logger.rs
+++ examples/yew-app/src/routes/hooks/use_logger.rs
@@ -5,10 +5,11 @@ use yew_hooks::{use_counter, use_logger, use_logger_eq, use_toggle};
 /// `use_logger` demo
 #[function_component(MyComponent)]
 fn my_component(props: &MyComponentProps) -> Html {
-    // Demo #1,
+    // Demo #1, logs whenever anything is updated.
     use_logger("MyComponent Props".to_string(), props.clone());
 
     let counter = use_counter(0);
+    // Demo #2, logs only when `prev_state != next_state`.
     use_logger_eq("MyComponent States".to_string(), counter.clone());
 
     let onincrease = {
