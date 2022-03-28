cb901ef8c7c5178a2d7319c6d92116f79109dab8
Jet Li <jing.i.qin@icloud.com>
2022-03-28 12:33:52 +0800

    Add use_debounce_state

---
+++ examples/yew-app/src/routes/hooks/use_debounce_state.rs
@@ -0,0 +1,40 @@
+use web_sys::HtmlInputElement;
+
+use yew::prelude::*;
+
+use yew_hooks::use_debounce_state;
+
+/// `use_debounce_state` demo
+#[function_component(UseDebounceState)]
+pub fn debounce_state() -> Html {
+    let value = use_state(|| "".to_string());
+    let debounced_value = use_debounce_state(|| "".to_string(), 2000);
+
+    let oninput = {
+        let value = value.clone();
+        let debounced_value = debounced_value.clone();
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            // This will update state every time.
+            value.set(input.value());
+            // This will delay updating state.
+            debounced_value.set(input.value());
+        })
+    };
+
+    html! {
+        <div class="app">
+            <header class="app-header">
+                <div>
+                    <input type="text" value={(*value).clone()} placeholder="Debounced input" {oninput}/>
+                    <p>
+                        <b>{ "Value: " }</b> {&*value}
+                    </p>
+                    <p>
+                        <b>{ "Debounced value: " }</b> {&*debounced_value}
+                    </p>
+                </div>
+            </header>
+        </div>
+    }
+}
